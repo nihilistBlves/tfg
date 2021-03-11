80f7041f1c9b1f666d0326001d7ed2da04ac07de
nihilistBlves <alejo.perez.benayas@gmail.com>
2021-03-11 14:50:41 +0100

    Inicialización

---
+++ src/test/java/org/tfg/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
