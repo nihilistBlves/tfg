de581b15b78a92bfbed18ddc2ab27e5eefb1cb01
nihilist_blves <alejo.perez.benayas@gmail.com>
2021-03-22 11:04:22 +0100

    Añadidos Domains: Admin, Mensaje, Rol y Usuario para demostrar el uso a otros compañeros

---
+++ src/main/java/org/tfg/domain/Rol.java
@@ -0,0 +1,27 @@
+package org.tfg.domain;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Rol {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	@Column(unique = true)
+	String tipo;
+	
+	@OneToMany(mappedBy = "rol",cascade = CascadeType.ALL)
+	private Collection<Usuario> usuarios;
+	
+	
+}
