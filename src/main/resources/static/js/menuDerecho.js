9fa890e84e7d40cfbfeb7d68a0975d6992fec3c4
david <david@DESKTOP-T6D93DV>
2021-04-02 21:31:11 +0200

    menu derecho funcional

---
+++ src/main/resources/static/js/menuDerecho.js
@@ -0,0 +1,17 @@
+  $(document).ready(function () {
+
+            $("#aparecer").hide();
+
+            $("#boton").click(function () {
+
+                $("#aparecer").show();
+
+                $("#opcion").click(function () {
+
+                    $("#aparecer").hide();
+
+                });
+
+
+            });
+        });
