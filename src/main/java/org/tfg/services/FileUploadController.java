ce749fd11e61c3df338c51c9eb40f4650d850987
david <david@DESKTOP-T6D93DV>
2021-04-18 20:47:42 +0200

    subida de archivos realizada . Falta comprobar si la carpeta existe o no y donde alojar los archivos. Y elegir que tipo de archivos poder subir

---
+++ src/main/java/org/tfg/services/FileUploadController.java
@@ -0,0 +1,58 @@
+package org.tfg.services;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+
+	@GetMapping("/")
+	public String index() {
+		return "upload";
+	}
+
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes)
+			throws IOException {
+
+		//Se comprueba si el archivo no es nulo o vacio si es asi se redirige con un mensaje a la otra web
+		// con redirect atributes hacemos un sistema parecido a prg donde llevamos al usuario a otra pagina con un mensaje
+		if (file == null || file.isEmpty()) {
+			attributes.addFlashAttribute("message", "Por favor seleccione un archivo");
+			return "redirect:status";
+		}
+		
+		//CREAR LA CARPETA Y RUTAS
+		
+				//TAREA=====
+		
+			//crear carpeta si no existe y hacer que no se repitan nombres o prohibir ciertos tipos de terminaciones
+
+		byte[] fileBytes = file.getBytes();
+
+		Path path = Paths.get("src//main//resources//static/multimedia");
+		String rutaAbsoluta=path.toFile().getAbsolutePath();
+		Path rutaCompleta=Paths.get(rutaAbsoluta+"//"+file.getOriginalFilename());
+		Files.write(rutaCompleta, fileBytes);
+		
+		
+		attributes.addFlashAttribute("message", "Archivo cargado correctamente ["+rutaCompleta+"]");
+
+		return "redirect:/status";
+	}
+	
+	@GetMapping("/status")
+	public String status() {
+		return "status";
+	}
+
+}
