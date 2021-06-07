772c41f3a7bb898b7cc1fb829a843fdca9c79b59
dani <dani@DESKTOP-D1MSM17>
2021-06-07 21:20:35 +0200

    Solucionado tasa waves FIN

--- src/main/java/org/tfg/repositories/WaveRepository.java
+++ src/main/java/org/tfg/repositories/WaveRepository.java
@@ -1,5 +1,6 @@
 package org.tfg.repositories;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,6 +20,44 @@ public interface WaveRepository extends JpaRepository<Wave, Long>{
 	@Query(value = "SELECT * FROM wave WHERE wave.publicacion_waved_id = :idPublicacion AND wave.da_wave_id = :idUsuario"
 			+ "", nativeQuery = true)
 	public Wave wavesDados(@Param("idPublicacion") Long idPublicacion, @Param("idUsuario") Long idUsuario);
-
+	
+	//QUERY: select * from wave where (fecha_like between "2021-06-01" and "2021-07-01") and (da_wave_id = 1)
+	
+	//Enero
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-01-01' AND '2021-02-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByEne(@Param("idUsuario") Long idUsuario);
+	//Febrero
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-02-01' AND '2021-03-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByFeb(@Param("idUsuario") Long idUsuario);
+	//Marzo
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-03-01' AND '2021-04-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByMar(@Param("idUsuario") Long idUsuario);
+	//Abril
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-04-01' AND '2021-05-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByAbr(@Param("idUsuario") Long idUsuario);
+	//Mayo
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-05-01' AND '2021-06-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByMay(@Param("idUsuario") Long idUsuario);
+	//Junio
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-06-01' AND '2021-07-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByJun(@Param("idUsuario") Long idUsuario);
+	//Julio
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-07-01' AND '2021-08-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByJul(@Param("idUsuario") Long idUsuario);
+	//Agosto
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-08-01' AND '2021-09-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByAgo(@Param("idUsuario") Long idUsuario);
+	//Septiembre
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-09-01' AND '2021-10-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedBySep(@Param("idUsuario") Long idUsuario);
+	//Octubre
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-10-01' AND '2021-11-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByOct(@Param("idUsuario") Long idUsuario);
+	//Noviembre
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-11-01' AND '2021-12-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByNov(@Param("idUsuario") Long idUsuario);
+	//Diciembre
+	@Query(value = "SELECT wave.id as wid FROM wave INNER JOIN publicacion ON wave.publicacion_waved_id = publicacion.id AND publicacion.duenio_publicacion_id = :idUsuario AND wave.fecha_like BETWEEN '2021-12-01' AND '2022-01-01'", nativeQuery = true)
+	public Long[] idsPublicacionWavedByDic(@Param("idUsuario") Long idUsuario);
 	
 }
