2c63b8c71dda9cc4213634817118e047a331aa6e
nihilist_blves <alejo.perez.benayas@gmail.com>
2021-06-16 12:29:23 +0200

    Faltaba una condicion

--- src/main/java/org/tfg/helper/H.java
+++ src/main/java/org/tfg/helper/H.java
@@ -35,7 +35,9 @@ public class H {
 	}
 	
 	public static void actualizarPeticiones (HttpSession s, SeguimientoRepository sr) {
-		s.setAttribute("numPeticiones", sr.findSeguidoresNoAceptadosByIdUsuario(((Usuario) s.getAttribute("userLogged")).getId()).size());
+		if (s.getAttribute("userLogged") != null) {
+			s.setAttribute("numPeticiones", sr.findSeguidoresNoAceptadosByIdUsuario(((Usuario) s.getAttribute("userLogged")).getId()).size());
+		}
 	}
 	
 	public static SerialBlob convertidorBlob(MultipartFile file) throws IOException, SerialException, SQLException{
